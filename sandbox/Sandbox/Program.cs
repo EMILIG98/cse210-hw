802aa8286f50b232cdad1daaad61d7f05205fec7
EmilioGarcia <emilio.e.garcia.rt@gmail.com>
2025-10-31 15:01:35 -0500

    hello

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -3,7 +3,10 @@ using System;
 class Program
 {
     static void Main(string[] args)
+
     {
+        // hello Im Emilio 
+        
         Console.WriteLine("Hello World!!!!!! This is the  Project.");
 
     }
