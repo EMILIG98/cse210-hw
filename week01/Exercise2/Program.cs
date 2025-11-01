53bd05ea6c0250e4dd3b8abb2b9244a3e10cafaa
EmilioGarcia <emilio.e.garcia.rt@gmail.com>
2025-11-01 18:38:35 -0500

    finished ecercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Wat is your grade percentage");
+        int percent = int.Parse(Console.ReadLine());
+
+        string letter = "";
+        string sing = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+
+        }
+        else
+        {
+            letter = "F";
+
+        }
+
+        Console.WriteLine($"\nYour grade is: {letter}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed! Congratulations!! you are the best !!!");
+
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time !!");
+
+        }
     }
+
 }
