8ec4b1e1474bff7afd353977d0dabffd54851ed4
EmilioGarcia <emilio.e.garcia.rt@gmail.com>
2025-11-09 21:04:56 -0500

    Journal

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Journal journal = new Journal();
+        string choice = "";
+
+        while (choice != "5")
+        {
+            Console.WriteLine("Please select one of the following choices:");
+            Console.WriteLine("1. Write a new entry");
+            Console.WriteLine("2. Display the journal");
+            Console.WriteLine("3. Load the journal");
+            Console.WriteLine("4. Save the journal");
+            Console.WriteLine("5. Quit");
+            Console.Write("What would you like to do? ");
+            choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                journal.AddEntry();
+            }
+            else if (choice == "2")
+            {
+                journal.DisplayAll();
+            }
+            else if (choice == "3")
+            {
+                Console.Write("What is the filename? ");
+                string filename = Console.ReadLine();
+                journal.LoadFromFile(filename);
+            }
+            else if (choice == "4")
+            {
+                Console.Write("What is the filename? ");
+                string filename = Console.ReadLine();
+                journal.SaveToFile(filename);
+            }
+            else if (choice == "5")
+            {
+                Console.WriteLine("Goodbye!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice, please try again.");
+            }
+
+            Console.WriteLine();
+        }
     }
 }
