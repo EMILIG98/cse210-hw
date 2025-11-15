e095566e3991733512bd6cfb6541a6087d897844
EmilioGarcia <emilio.e.garcia.rt@gmail.com>
2025-11-14 20:26:54 -0500

    Scripture

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+
+        foreach (var word in text.Split(" "))
+        {
+            _words.Add(new Word(word));
+        }
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        Random random = new Random();
+
+        for (int i = 0; i < numberToHide; i++)
+        {
+            int index = random.Next(_words.Count);
+            _words[index].Hide();
+
+        }
+    }
+    
+
+    public string GetDisplayText()
+    {
+        string result = _reference.GetDisplayText() + " ";  
+        
+        foreach (var word in _words)
+        {
+            result += word.GetDisplayText() + " ";
+
+        }
+        return result;
+    }
+    public bool IsCompletelyHidden()
+    {
+        foreach (var word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+     
+
+         
+}
