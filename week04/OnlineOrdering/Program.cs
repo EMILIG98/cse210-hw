799cd5f1eb51bd3363076e78c273108d538bf8d5
EmilioGarcia <emilio.e.garcia.rt@gmail.com>
2025-11-27 15:38:25 -0500

    Online

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+        Address a1 = new Address("123 Main St", "Miami", "FL", "USA");
+        Customer c1 = new Customer("John Smith", a1);
+
+        Order order1 = new Order(c1);
+        order1.AddProduct(new Product("Laptop", "LP100", 900, 1));
+        order1.AddProduct(new Product("Mouse", "MS200", 20, 2));
+
+        
+        Address a2 = new Address("Avenida Central", "Quito", "Pichincha", "Ecuador");
+        Customer c2 = new Customer("Emilio Garcia", a2);
+
+        Order order2 = new Order(c2);
+        order2.AddProduct(new Product("Phone", "PH500", 400, 1));
+        order2.AddProduct(new Product("Charger", "CH100", 15, 3));
+
+       
+        Console.WriteLine(order1.GetPackingLabel());
+        Console.WriteLine(order1.GetShippingLabel());
+        Console.WriteLine($"TOTAL PRICE: ${order1.GetTotalPrice()}\n");
+
+        Console.WriteLine(order2.GetPackingLabel());
+        Console.WriteLine(order2.GetShippingLabel());
+        Console.WriteLine($"TOTAL PRICE: ${order2.GetTotalPrice()}\n");
     }
 }
+    
